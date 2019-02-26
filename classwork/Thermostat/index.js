dfd85e0bd3e20e3a2839cc0db2f26c2745f6d707
DeadAugust <augustluhrs@gmail.com>
2019-02-26 13:45:50 -0500

    client test

--- classwork/Thermostat/index.js
+++ classwork/Thermostat/index.js
@@ -7,10 +7,62 @@
   based on tigoe/NodeServerExamples:
     rotary-encoder
     gpio-input
+    SimpleConnDevClient
 
-  and momenso's rpi-dht-sensor
-  https://github.com/momenso/node-dht-sensor
+  and this rpi-dht-sensor
+  https://www.npmjs.com/package/rpi-dht-sensor
 */
+
+//client stuff
+const https = require('https');
+var hostName = 'tigoe.io'; // add the host name here
+var temperature = 0.0;
+var humidity = 0.0;
+
+// make the sensor data a JSON object:
+var sensorData = JSON.stringify({
+    'temperature': temperature,
+    'humidity': humidity
+});
+
+// make the POST data a JSON object and stringify it:
+var postData =JSON.stringify({
+  'macAddress': 'b8:27:eb:dd:b8:d5',    // add your mac address here
+  'sessionKey': '4e233ce4-946c-47a5-8102-dfb3c09bfe83',    // add your session key here
+  'data': sensorData
+});
+
+var options = {
+  host: hostName,
+  port: 443,
+  path: '/data',
+	method: 'POST',
+	headers: {
+    'User-Agent': 'nodejs',
+    'Content-Type': 'application/json',
+    'Content-Length': postData.length
+  }
+};
+
+function callback(response) {
+  var result = '';		// string to hold the response
+
+  // as each chunk comes in, add it to the result string:
+  response.on('data', function (data) {
+    result += data;
+  });
+
+  // when the final chunk comes in, print it out:
+  response.on('end', function () {
+    console.log(result);
+  });
+}
+
+// make the actual request:
+var request = https.request(options, callback);	// start it
+request.write(postData);						// send the data
+request.end();									    // end it
+console.log('checking data\n' + postData); //just to check
 
 //gpio set up
 let Gpio = require('onoff').Gpio; //onoff library
@@ -58,13 +110,16 @@ function readRotation(direction){
 
 //dht11 set up
 var rpiDhtSensor = require('rpi-dht-sensor');
-var dht = new rpiDhtSensor.DHT11(2); //actual pin not GPIO
+var dht = new rpiDhtSensor.DHT11(2); // GPIO pin?
 function read () {
   var readout = dht.read();
     console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
         'humidity: ' + readout.humidity.toFixed(2) + '%');
+    temperature = readout.temperature.toFixed(2);
+    humidity = readout.humidity.toFixed(2);
     setTimeout(read, 5000);
 }
+
 
 //terminal signals start
 console.log('on');
