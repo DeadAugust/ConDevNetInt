66b4c29c14833cd1092ba46becce5f2235ce3e07
DeadAugust <augustluhrs@gmail.com>
2019-02-25 23:03:40 -0500

    LEDs and Encoder example test

---
+++ classwork/Thermostat/index.js
@@ -0,0 +1,50 @@
+/* Web-Connected Thermostat
+  by August Luhrs
+  Feb. 25 2019
+  for Connected Devices and Networked Interactions
+  Tom Igoe -- ITP Spring 2019
+
+  based on tigoe/NodeServerExamples:
+    rotary-encoder
+    gpio-input
+*/
+
+//gpio set up
+let Gpio = require('onoff').Gpio; //onoff library
+let yellow1 = new Gpio(25, 'out');
+let yellow2 = new Gpio(8, 'out'); //set pins
+let yellow3 = new Gpio(7, 'out');
+let green = new Gpio(12, 'out');
+//led states
+let greenState = 0;
+let yellowStates = [0, 0, 0]; //cycling through yellow LEDs
+//encoder set up
+const rotaryEncoder = require('onoff-rotary'); //encoder library
+const myEncoder = rotaryEncoder(20,21); //encoder input pins
+//encoder listener function
+function readRotation(direction){
+  console.log(direction);
+  if (direction > 0) {
+      console.log('Encoder rotated right');
+      for (var i = yellowStates.length - 1; i >= 0; i--){
+        if (yellowStates[i] == 1 && i != 2){
+          yellowStates[i] = 0;
+          yellowStates[i+1] = 1;
+      }
+  } else {
+      console.log('Encoder rotated left');
+      for (var i = 0; i < yellowStates.length; i++){
+        if (yellowStates[i] == 1 && i != 0){
+          yellowStates[i] = 0;
+          yellowStates[i-1] = 1;
+      }
+  }
+  //led array cycle
+  yellow1.writeSync(yellowStates[0]);
+  yellow2.writeSync(yellowStates[1]);
+  yellow3.writeSync(yellowStates[2]);
+  greenState = !greenState;
+  green.writeSync(greenState);
+}
+//start the encoder event listener
+myEncoder.on('rotation', readRotation);
